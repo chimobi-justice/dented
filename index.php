160b2c18fb5e9ac7b29f5471dbbc207c715761a2
justice chimobi <chimobi.justice@gmail.com>
2021-02-12 08:22:45 -0600

    dented nav/herosection

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dented</title>
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/styles/index.css">    
+</head>
+<body>
+<!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+  <a class="navbar-brand" href="#"><i>Dented</i></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Browse all jobs <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Find a job</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Post a job</a>
+      </li>
+    </ul>
+    <ul class="navbar-nav">
+      <li><a href="#" class="btn btn-md" id="loginBtn"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+  </div>
+</nav> -->
+  <!--  -->
+    <nav id="navbar">
+      <div id="container">
+      <div class="brand-holder">
+        <h2><i><a class="brand" href="#">Dented</a></i></h2>
+      </div>
+      <ul>
+        <li><a href="#">Find a job</a></li>
+        <li><a href="#">Browse all jobs</a></li>
+        <li><a href="#">Post a job</a></li>
+      </ul>
+      <div class="loginBtn-holder">
+        <button class="btn btn-md c-my-btn">login</button>
+      </div>
+    </div>
+  </nav>
+
+  <div id="hero-section">
+    <div class="hero-wrapper">
+      <div>
+        <h1>Find World's Best</h1>
+        <h1>Remote Job</h1>
+        <p>After coronavirus outbreak everyone search for remote jobs. Also employers wnats to get done their works through outsource.Here is the Solution</p>
+        <button class="btn btn-lg c-my-btn">Get Started</button>
+      </div>
+      <div>
+        <img class="img-responsive img-circle" src="assets/images/herosection-banner-img2.png" alt="hero_banner">
+      </div>
+    </div>
+  </div>
+
+  <div class="container bg-white mt-5 mb-5 p-3 text-info">
+    <h2>Hiring? Candidate are looking for you.</h2>
+    <div class="row">
+      <div class="col-3 text-center">
+        <h3>567</h3>
+        <h4>Average job applicate</h4>
+      </div>
+      <div class="col-3 text-center">
+        <h3>567</h3>
+        <h4>Average Days to Hire</h4>
+      </div>
+      <div class="col-3 text-center">
+        <h3>567</h3>
+        <h4>Newsletter Subscribers</h4>
+      </div>
+      <div class="col-3 text-center">
+        <button class="btn btn-lg c-my-btn">Post a job</button>
+      </div>
+
+    </div>
+  </div>
+
+  <script src="./bootstrap/js/jquery.min.js"></script>
+  <script src="./bootstrap/js/bootstrap.bundle.js"></script>
+  <script src="./bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+</html>
