56a5e77e7f1e4f7a9d84bfc5016091cef36f823f
justice chimobi <chimobi.justice@gmail.com>
2021-02-13 12:08:14 -0600

    added trending-jobs

---
+++ category.php
@@ -0,0 +1,158 @@
+<div class="container-fluid">
+    <div class="container d-flex justify-content-between text-dark category-heading pt-5">
+      <div>
+        <h1>Remote Jobs</h1>
+        <h1>By Category</h1>
+      </div>
+      <div class="pt-5">
+        <p class="p-5">Explore Category</p>
+      </div>
+    </div>
+  <div class="category_cards">
+    <div>
+      <div class="d-flex justify-content-between">
+        <div>
+          <img src="assets/images/myicon.png" class="img-circle"> 
+        </div>  
+        <div>
+          <pre>
+          ..........
+          ..........
+          ..........
+          ..........
+          </pre>
+        </div>   
+      </div>     
+      <h5>Costumer Support</h5>
+      <p>14 job vacancy</p>
+      <button class="btn btn-sm category-btn">Know More</button>
+    </div>
+    <div>
+      <div class="d-flex justify-content-between">
+        <div>
+          <img src="assets/images/myicon.png" class="img-circle"> 
+        </div>  
+        <div>
+          <pre>
+          ..........
+          ..........
+          ..........
+          ..........
+          </pre>
+        </div>   
+      </div>     
+      <h5>Costumer Support</h5>
+      <p>14 job vacancy</p>
+      <button class="btn btn-sm category-btn">Know More</button>
+    </div>
+    <div>
+      <div class="d-flex justify-content-between">
+        <div>
+          <img src="assets/images/myicon.png" class="img-circle"> 
+        </div>  
+        <div>
+         <pre>
+          ..........
+          ..........
+          ..........
+          ..........
+          </pre>
+        </div>   
+      </div>     
+      <h5>Costumer Support</h5>
+      <p>14 job vacancy</p>
+      <button class="btn btn-sm  category-btn">Know More</button>
+    </div>
+    <div>
+      <div class="d-flex justify-content-between">
+        <div>
+          <img src="assets/images/myicon.png" class="img-circle"> 
+        </div>  
+        <div>
+          <pre>
+          ..........
+          ..........
+          ..........
+          ..........
+          </pre>
+        </div>   
+      </div>     
+      <h5>Costumer Support</h5>
+      <p>14 job vacancy</p>
+      <button class="btn btn-sm category-btn">Know More</button>
+    </div>
+  </div>
+  <div class="category_cards">
+    <div>
+      <div class="d-flex justify-content-between">
+        <div>
+          <img src="assets/images/myicon.png" class="img-circle"> 
+        </div>  
+        <div>
+          <pre>
+          ..........
+          ..........
+          ..........
+          ..........
+          </pre>
+        </div>   
+      </div>     
+      <h5>Costumer Support</h5>
+      <p>14 job vacancy</p>
+      <button class="btn btn-sm category-btn">Know More</button>
+    </div>
+    <div>
+      <div class="d-flex justify-content-between">
+        <div>
+          <img src="assets/images/myicon.png" class="img-circle"> 
+        </div>  
+        <div>
+          <pre>
+          ..........
+          ..........
+          ..........
+          ..........
+          </pre>
+        </div>   
+      </div>     
+      <h5>Costumer Support</h5>
+      <p>14 job vacancy</p>
+      <button class="btn btn-sm category-btn">Know More</button>
+    </div>
+    <div>
+      <div class="d-flex justify-content-between">
+        <div>
+          <img src="assets/images/myicon.png" class="img-circle"> 
+        </div>  
+        <div>
+          <pre>
+          ..........
+          ..........
+          ..........
+          ..........
+          </pre>
+        </div>   
+      </div>     
+      <h5>Costumer Support</h5>
+      <p>14 job vacancy</p>
+      <button class="btn btn-sm category-btn">Know More</button>
+    </div>
+    <div>
+      <div class="d-flex justify-content-between">
+        <div>
+          <img src="assets/images/myicon.png" class="img-circle"> 
+        </div>  
+        <div>
+          <pre>
+          ..........
+          ..........
+          ..........
+          ..........
+          </pre>
+        </div>   
+      </div>     
+      <h5>Costumer Support</h5>
+      <p>14 job vacancy</p>
+      <button class="btn btn-sm category-btn">Know More</button>
+    </div>
+  </div>
